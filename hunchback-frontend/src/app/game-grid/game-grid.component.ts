fae524180a02954d5489419b41519fcecfb91717
Emil Olsson <emil.olsson@tacton.com>
2017-12-14 20:14:01 +0100

    Added basic grid ui

---
+++ hunchback-frontend/src/app/game-grid/game-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-grid',
+  templateUrl: './game-grid.component.html',
+  styleUrls: ['./game-grid.component.css']
+})
+export class GameGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
