899d6c604c3262f761957f8a6f5505f89280d76c
Ove Noros <ove.noros@tillvaxtverket.se>
2018-01-21 15:03:19 +0100

    Fixed repeating inputs

--- hunchback-dancepad/dancePad.py
+++ hunchback-dancepad/dancePad.py
@@ -5,7 +5,6 @@ import time
 arrows = {31: 'up', 47: 'down', 79: 'left', 143: 'right'}
 other = {1: 'Triangle', 2: 'Square', 4: 'X', 8: 'O', 16: 'Select', 32: 'Start'}
 
-
 def send_request(direction):
     resp = requests.get('http://192.168.86.27:8080/direction/{}/'.format(direction))
     if resp.status_code != 200:
@@ -16,21 +15,25 @@ def send_request(direction):
 dev = usb.core.find(idVendor=0x79, idProduct=0x11)
 interface = 0
 endpoint = dev[0][(0,0)][0]
+previous = ''
 
 if dev.is_kernel_driver_active(interface) is True:
   dev.detach_kernel_driver(interface)
   usb.util.claim_interface(dev, interface)
 while True:
-    time.sleep(50/100)
     try:
         data = dev.read(endpoint.bEndpointAddress,endpoint.wMaxPacketSize)
         col5 = data[5]
         col6 = data[6]
         if col5 != 15:
-            print(arrows.get(col5))
-            send_request(arrows.get(col5))
+            if previous != col5:
+                print(arrows.get(col5))
+                send_request(arrows.get(col5))
+                previous = col5
         elif col6 != 0:
             print(other.get(col6))
+        else:
+            previous = ''
     except usb.core.USBError as e:
         data = None
         if e.args == ('Operation timed out',):
