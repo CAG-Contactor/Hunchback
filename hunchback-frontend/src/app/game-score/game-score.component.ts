a66503ee1325657cd1f3d9969c16c73b14fa8767
Joel Binnquist <joel.binnquist@gmail.com>
2018-02-05 20:48:08 +0100

    score

--- hunchback-frontend/src/app/game-score/game-score.component.ts
+++ hunchback-frontend/src/app/game-score/game-score.component.ts
@@ -1,8 +1,10 @@
-import {Component, OnInit, Input} from '@angular/core';
-import {BackendService} from "../backend.service";
-import {Subscription} from "rxjs/Subscription";
-import {ScoreCard} from "../game-model/score-card";
-import {NgModel} from "@angular/forms";
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { BackendService } from '../backend.service';
+import { ScoreCard } from '../game-model/score-card';
 
 // export interface ScoreCard {
 //   messageType: string,
@@ -19,7 +21,7 @@ import {NgModel} from "@angular/forms";
 @Component({
   selector: 'app-game-score',
   templateUrl: './game-score.component.html',
-  styleUrls: ['./game-score.component.css']
+  styleUrls: ['./game-score.component.scss']
 })
 export class GameScoreComponent implements OnInit {
   subscription: Subscription;
@@ -32,24 +34,27 @@ export class GameScoreComponent implements OnInit {
 
   newScoreCard() {
     this.scoreCard = {
-      userName: "",
+      userName: '',
       score: 0
     };
   }
 
   sendScoreCard() {
-    this.backendService.addScore(this.scoreCard).subscribe(res => {
-      this.scoreCard = undefined;
-      this.backendService.getHighScores().subscribe(m => {
-        if (m.messageType === 'HighScores') {
-          this.highScores = m.highScores;
+    this.backendService
+      .addScore(this.scoreCard)
+      .subscribe(res => {
+          this.scoreCard = undefined;
+          this.backendService.getHighScores()
+            .subscribe(m => {
+              if (m.messageType === 'HighScores') {
+                this.highScores = m.highScores;
+              }
+            });
+        },
+        err => {
+          console.error(err);
         }
-      });
-      },
-      err => {
-      console.error(err);
-      }
-    );
+      );
 
   }
 
